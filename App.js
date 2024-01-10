865a64b18225d6dded03bca6601016d61a77410c
Jahnavi Singh Chauhan <jahnavi.singhchauhan@studentambassadors.com>
2024-01-10 22:46:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+// App.js
+import React from 'react';
+import './App.css';
+import Advertisement from './Advertisement'; // Import the Advertisement component
+
+function App() {
+    return (
+        <div className="App">
+            {/* Other existing content */}
+            <Advertisement /> {/* Add your Advertisement component */}
+        </div>
+    );
+}
+
+export default App;
+
+
